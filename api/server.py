c8d9eafc51f1e0d00430568b4b106b21ed5a9e2a
joren.sjongers <joren.sjongers@icloud.com>
2019-07-10 16:18:33 +0200

    Simple API that returns geosjon containing features that are older than given date

--- api/server.py
+++ api/server.py
@@ -1,18 +1,43 @@
 from flask import Flask
 from flask import request
+import json
+from geojson import FeatureCollection
 
 app = Flask(__name__)
 
-
+# annotation specifies route
 @app.route("/api/map/historical")
-def hello():
-    kind = request.args.get('kind')
+def getMatchedFeaturesHistorical():
+    # extract argument from request
     date = request.args.get('date')
     
-    with open('match_dates/bike_infra.geojson', 'r') as sourceDate:
-        
+    # load geodata
+    with open('match_dates/bike_infra.geojson', 'r') as sourceFile:
+        sourceData = json.loads(sourceFile.read())
+
+    # load construction year data
+    with open('match_dates/construction_year.json', 'r') as sourceFile:
+        constructionYears = json.loads(sourceFile.read())
+
+    # generate list of correct gids
+    gids = []
+    for gid in constructionYears.keys():
+        if int(constructionYears[gid]) <= int(date):
+            gids.append(int(gid))
 
 
+    # select fautures older than or as ols as the give date
+    requestedFeatures = []
+    for feature in sourceData['features']:
+        if feature['properties']['gid'] in gids:
+            requestedFeatures.append(feature)
+
+
+    # create geojson
+    collection = FeatureCollection(requestedFeatures)
+
+    # return geojson as string
+    return json.dumps(collection)
 
 if __name__ == "__main__":
     app.run(debug=True)

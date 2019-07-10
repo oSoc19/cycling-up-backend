0a7479c7ce4aca17a1cf76a6a0b8483712d9d9ea
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-10 03:30:45 +0200

    Renaming api server file

--- api/server.py
+++ api/server.py
@@ -3,8 +3,13 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route("/api/")
 def hello():
     kind = request.args.get('kind')
     date = request.args.get('date')
     return "Hello world!" + kind + date
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

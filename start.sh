e058f1f779ea90793239c912d70682527fc230f3
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-12 15:08:10 +0200

    Fix incorrect args

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ app_name="cycling-up-api"
 external_port=5005
 flask_port=5000
 
-docker build --rm -f "Dockerfile" -t ${app_name} .
+docker build --rm -f "Dockerfile" --tag ${app_name} .
 
 docker run --rm \
     --publish ${external_port}:${flask_port} \
     --name=${app_name} \
-    --tag=${app_name} \
     ${app_name}

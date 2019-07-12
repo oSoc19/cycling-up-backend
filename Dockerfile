d7f4b9532aa1b3ab8c57823662d3d3a923261be5
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-12 15:13:39 +0200

    Fix incorrect params for Dockerfile CMD

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pipenv install --system --deploy
 RUN chmod +x app.py
 
 # CMD ["python", "app.py"]
-CMD ["gunicorn", "--workers=6", "--bind=0.0.0.0:5000", "--name=backend", "app:configure_app()"]
+CMD ["gunicorn", "--workers=6", "--bind=0.0.0.0:5000", "--name=backend", "app:api"]
--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from flask_cors import CORS
 import api.getters as getters
 
 
-app = Flask(__name__)
+api = Flask(__name__)
 
 # Compress each response which content-length > 600
-Compress(app)
+Compress(api)
 
 # Enable CORS headers on each api routes
-CORS(app, allow_headers="Content-Type")
+CORS(api, allow_headers="Content-Type")
 
 
-@app.route("/")
-@app.route("/ping")
-@app.route("/api/ping")
+@api.route("/")
+@api.route("/ping")
+@api.route("/api/ping")
 def api_ping():
     """
     Route to check the connectivity
@@ -26,7 +26,7 @@ def api_ping():
 
 
 ### historical infra map ###
-@app.route("/api/map/historical/")
+@api.route("/api/map/historical/")
 def getHistoricalMap():
     # extract argument from request
     date = request.args.get("date")
@@ -34,7 +34,7 @@ def getHistoricalMap():
 
 
 ### general map ###
-@app.route("/api/map/general/<kind>")
+@api.route("/api/map/general/<kind>")
 def getGeneralMap(kind):
     data = getters.getJsonContents(kind)
     if data is not None:
@@ -44,7 +44,7 @@ def getGeneralMap(kind):
 
 
 # file not found error
-@app.errorhandler(404)
+@api.errorhandler(404)
 def _not_found(msg="File not found!"):
     message = {"status": 404, "message": msg}
     resp = jsonify(message)
@@ -53,10 +53,10 @@ def _not_found(msg="File not found!"):
     return resp
 
 
-def configure_app():
-    return app
+def configure_api():
+    return api
 
 
 if __name__ == "__main__":
-    configure_app()
-    app.run(debug=True, host="0.0.0.0")
+    configure_api()
+    api.run(debug=True, host="0.0.0.0")

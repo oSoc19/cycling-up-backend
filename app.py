56a616a3ea207b93058895b03bd9142db706f548
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-12 14:13:33 +0200

    Add host address

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ app = Flask(__name__)
 Compress(app)
 
 # Enable CORS headers on each api routes
-CORS(app, allow_headers='Content-Type')
+CORS(app, allow_headers="Content-Type")
 
 
-@app.route('/')
-@app.route('/ping')
-@app.route('/api/ping')
+@app.route("/")
+@app.route("/ping")
+@app.route("/api/ping")
 def api_ping():
     """
     Route to check the connectivity
@@ -25,41 +25,38 @@ def api_ping():
     return jsonify(message="Yello World !")
 
 
-
 ### historical infra map ###
 @app.route("/api/map/historical/")
-def getMapHistorical():
+def getHistoricalMap():
     # extract argument from request
-    date = request.args.get('date')
+    date = request.args.get("date")
     return jsonify(getters.getMatchedFeaturesHistorical(date))
+
 
 ### general map ###
 @app.route("/api/map/general/<kind>")
-def getMapBicyclePump(kind):
+def getGeneralMap(kind):
     data = getters.getJsonContents(kind)
     if data is not None:
         return jsonify(data)
     else:
-        return not_found()
+        return _not_found()
 
 
 # file not found error
 @app.errorhandler(404)
-def not_found():
-    message = {
-        'status': 404,
-        'message': 'File not found!'
-    }
+def _not_found(msg="File not found!"):
+    message = {"status": 404, "message": msg}
     resp = jsonify(message)
     resp.status_code = 404
 
     return resp
 
 
-
 def configure_app():
     return app
 
+
 if __name__ == "__main__":
     configure_app()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")

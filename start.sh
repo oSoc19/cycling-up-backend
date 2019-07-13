ba98b522359d33d1a78a46e5f6ff50877c80feda
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-13 06:28:43 +0200

    Add .env example

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Load environment variables from .env
+if [ ! -f .env ]
+then
+	set -o allexport
+	source .env
+	set +o allexport
+fi
+
+
 app_name="cycling-up-api"
 external_port=5005
 flask_port=5000

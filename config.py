574dfca09f4846cf4b16c5de795a8e7b468b58cf
Dakad <3106338+Dakad@users.noreply.github.com>
2019-07-15 16:25:24 +0200

    Update config

--- config.py
+++ config.py
@@ -1,27 +1,26 @@
 import os
+from pathlib import Path
+
 
 _BASE_DIR = Path(__file__).parent
-_DATA_DIR = os.path.join(_BASE_DIR, "data")
+_DEFAULT_DATA_DIR = os.path.join(_BASE_DIR, "data")
+_DEFAULT_LOG_DIR = os.path.join(_BASE_DIR.parent, "logs")
 
 
 class Config(object):
-    ENV = os.getenv("FLASK_ENV", "production")
-    DEBUG = False
+    ENV = os.getenv("FLASK_ENV", default="production")
+    DEBUG = os.getenv("DEBUG", default="false").lower() in {"1", "t", "true"}
+    DATA_DIR = os.getenv("DATA_DIR", default=_DEFAULT_DATA_DIR)
+    LOG_DIR = os.getenv("LOG_DIR", default=_DEFAULT_LOG_DIR)
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class TestingConfig(Config):
-    DEBUG = True
-    TESTING = True
 
 
 class ProductionConfig(Config):
     DEBUG = False
 
 
-config_by_name = dict(dev=DevelopmentConfig, test=TestingConfig, prod=ProductionConfig)
+config_by_name = dict(dev=DevelopmentConfig, prod=ProductionConfig)
 

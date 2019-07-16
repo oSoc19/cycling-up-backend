78157000ae4fe1604b1c12d6a1e7212571e540cf
joren.sjongers <joren.sjongers@icloud.com>
2019-07-16 15:42:28 +0200

    added documentation

--- process_data/historic_data/make_count_json.py
+++ process_data/historic_data/make_count_json.py
@@ -3,10 +3,11 @@ import json
 import requests
 from geojson import Point, Feature, FeatureCollection
 
-MONTHS = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 
-            7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+# Define month names
+MONTHS = {1:'jan', 2:'feb', 3:'mar', 4:'apr', 5:'may', 6:'jun', 
+            7:'jul', 8:'aug', 9:'sep', 10:'oct', 11:'nov', 12:'dec'}
 
-
+# Match count place names to coordinates (done manually)
 PLACES = {  "Porte d'Anvers": [4.352713, 	50.856442],
             "Carrefour Loi/Colonie/Royale": [4.362422, 50.846872],
             "Place Stéphanie": [4.358225, 50.833603],
@@ -20,18 +21,20 @@ PLACES = {  "Porte d'Anvers": [4.352713, 	50.856442],
             "Rond-point de l'Université": [4.384273, 50.813779]  
 }
 
-
+# initialize all count locations as geojson features
 features = []
 for place in PLACES.keys():
     point = Point(PLACES[place])
-    properties = {'name': place, 'count_data': {'Jan': {}, 'May':{}, 'Sep': {}, 'Nov': {}}}
+    properties = {'name': place, 'count_data': {'jan': {}, 'may':{}, 'sep': {}, 'nov': {}}}
 
     features.append(Feature(geometry=point, properties=properties))
 
+# load csv data
 with open('process_data/historic_data/historic_count.csv', encoding='utf-8-sig') as source:
     reader = csv.reader(source, delimiter=";")
     headers = next(reader)
     
+    # extract info from row and place appropriately in geojson file.
     for row in reader:
         year = row[1]
         month = MONTHS[int(row[0])]
@@ -41,7 +44,9 @@ with open('process_data/historic_data/historic_count.csv', encoding='utf-8-sig')
             index += 1 
 
 
+# generate featurecollection
 result = FeatureCollection(features)
 
-with open('process_data/historic_data/result.json', 'w') as dest:
+# write results
+with open('process_data/historic_data/historic_bike_counts.json', 'w') as dest:
     json.dump(result, dest)

9d6c57ba4bb2008bdbdaaad5c55c87eb70c883d7
joren.sjongers <joren.sjongers@icloud.com>
2019-07-17 10:51:06 +0200

    add comments

--- process_data/make_historic_villo_json.py
+++ process_data/make_historic_villo_json.py
@@ -3,16 +3,16 @@ import json
 import requests
 
 data = []
+
+# load csv data
 with open('process_data/historic_data/yearly_villo_rentals.csv', 'r') as source:
     reader = csv.reader(source, delimiter=";")
     header = next(reader)
+    # build json as a list of dicts
     for row in reader:
         data.append({'year': int(row[0]),
                         'number_of_rentals': int(row[1])})
 
-
-result = {'name': 'total number of villo rentals by year',
-          'data': data}
-
+# write data
 with open('process_data/historic_data/villo_rentals.json', 'w') as dest:
-    json.dump(result, dest)
+    json.dump(data, dest)

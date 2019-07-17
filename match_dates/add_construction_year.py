7bc6f7471a7c8a25f8d06cd424d0325427726c1c
joren.sjongers <joren.sjongers@icloud.com>
2019-07-17 15:33:14 +0200

    update information

--- match_dates/add_construction_year.py
+++ match_dates/add_construction_year.py
@@ -1,12 +1,9 @@
 import json
 from geojson import FeatureCollection
 
-# Generates file containing geojson feature for which construction years are know
-# (match_dates/matched_features.json) and adds construction year as a property to this file.
-
-#########################
-# THIS FILE IS NOT USED #
-#########################
+# Generates file containing geojson feature for which construction years are known
+# (match_dates/data/matched_features.json) and adds construction year as a property to this file.
+# NOTE: This script is useless now, needed in an earlier stage of the project.
 
 result = {}
 

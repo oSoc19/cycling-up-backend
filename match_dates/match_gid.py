942a5078f792556853de044bf19abaee405878f1
joren.sjongers <joren.sjongers@icloud.com>
2019-07-17 15:25:51 +0200

    update file paths

--- match_dates/match_gid.py
+++ match_dates/match_gid.py
@@ -2,13 +2,13 @@ import json
 
 result = {}
 
-with open('match_dates/result_matched.geojson', 'r') as f:
+with open('match_dates/data/result_matched.geojson', 'r') as f:
     data = json.loads(f.read())
 
 for feature in data['features']:
     result[feature['properties']['gid']] = int(feature['properties']['construction year'])
 
 
-with open('match_dates/construction_year_by_gid.json', 'w') as f:
+with open('match_dates/data/construction_year_by_gid.json', 'w') as f:
     f.write(json.dumps(result))
 

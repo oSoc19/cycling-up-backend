055db1c27f9ef1ed18464e56670e993e43d5f15d
Jonathan Beliën <dev@jbelien.be>
2019-07-18 14:41:17 +0200

    Update make_count_json.py

--- process_data/make_count_json.py
+++ process_data/make_count_json.py
@@ -8,7 +8,7 @@ MONTHS = {1:'jan', 2:'feb', 3:'mar', 4:'apr', 5:'may', 6:'jun',
             7:'jul', 8:'aug', 9:'sep', 10:'oct', 11:'nov', 12:'dec'}
 
 # Match count place names to coordinates (done manually)
-PLACES = {  "Porte d'Anvers": [4.352713, 	50.856442],
+PLACES = {  "Porte d'Anvers": [4.352713, 50.856442],
             "Carrefour Loi/Colonie/Royale": [4.362422, 50.846872],
             "Place Stéphanie": [4.358225, 50.833603],
             "Mérode": [4.398920, 50.839202],

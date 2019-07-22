cecae9c4e010cd8654e5829a82b32e457a071b7d
joren.sjongers <joren.sjongers@icloud.com>
2019-07-22 19:41:29 +0200

    fix file refenences

--- process/charts/csv_by_year_to_json.py
+++ process/charts/csv_by_year_to_json.py
@@ -6,15 +6,13 @@ from pathlib import Path
 from config import get_config_by_env_mode
 
 BASE_DIR = Path(__file__).parent.parent
+CSV_DIR = os.path.join(BASE_DIR, "base_data", "csv_by_year")
 current_config = get_config_by_env_mode()
 
-def csv_by_year_to_json(filename):
-
+for csv in os.listdir(CSV_DIR):
     data = []
-
-    # load csv data
-    file_path = os.path.join(BASE_DIR, "base_data", filename + ".csv")
-    with open(file_path, 'r') as source:
+    # load csv data)
+    with open(csv, 'r') as source:
         reader = csv.reader(source, delimiter=";")
         header = next(reader)
         # build json as a list of dicts
@@ -23,5 +21,6 @@ def csv_by_year_to_json(filename):
                             header[1]: float(row[1])})
 
     # write json data
+    filename = csv.split('.')[0]
     with open(os.path.join(current_config.HISTORICAL_DIR, filename + ".json"), 'w') as dest:
         json.dump(data, dest)

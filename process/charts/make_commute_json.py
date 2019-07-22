5bda935a21d9723a762819c0159bf6286c73bf16
joren.sjongers <joren.sjongers@icloud.com>
2019-07-22 15:50:04 +0200

    commute -> commuting

--- process/charts/make_commute_json.py
+++ process/charts/make_commute_json.py
@@ -12,8 +12,8 @@ current_config = get_config_by_env_mode()
 BASE_DIR = Path(__file__).parent.parent
 
 data = {}
-with open(os.path.join(BASE_DIR, "base_data", "commute.json")) as source:
+with open(os.path.join(BASE_DIR, "base_data", "commuting.json")) as source:
     data = json.load(source)
 
-with open(os.path.join(current_config.HISTORICAL_DIR, 'commute.json'), 'w') as dest:
+with open(os.path.join(current_config.HISTORICAL_DIR, 'commuting.json'), 'w') as dest:
     json.dump(data, dest)

60057d48f33e1b7263f08416c7b73f0db00ffe65
joren.sjongers <joren.sjongers@icloud.com>
2019-07-22 19:46:04 +0200

    fix double varible names

--- process/charts/csv_by_year_to_json.py
+++ process/charts/csv_by_year_to_json.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).parent.parent
 CSV_DIR = os.path.join(BASE_DIR, "base_data", "csv_by_year")
 current_config = get_config_by_env_mode()
 
-for csv in os.listdir(CSV_DIR):
+for csv_data in os.listdir(CSV_DIR):
     data = []
     # load csv data)
-    with open(csv, 'r') as source:
+    with open(os.path.join(CSV_DIR, csv_data), 'r') as source:
         reader = csv.reader(source, delimiter=";")
         header = next(reader)
         # build json as a list of dicts
@@ -21,6 +21,6 @@ for csv in os.listdir(CSV_DIR):
                             header[1]: float(row[1])})
 
     # write json data
-    filename = csv.split('.')[0]
+    filename = csv_data.split('.')[0]
     with open(os.path.join(current_config.HISTORICAL_DIR, filename + ".json"), 'w') as dest:
         json.dump(data, dest)

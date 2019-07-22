9107a96ebc1cad5bc06899dbdd165a2a68acc201
joren.sjongers <joren.sjongers@icloud.com>
2019-07-22 14:21:44 +0200

    fix function call

--- process/fetch/__init__.py
+++ process/fetch/__init__.py
@@ -1,10 +1,15 @@
-
 import sys
 import os
 parent = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, parent)
 
 from config import get_config_by_env_mode
-import process.fetch.fetch_mobigis
+get_config_by_env_mode().init_dirs()
+
+# fetch mobigis map data
+import process.fetch.fetch_mobigis as fetch_mobigis
+fetch_mobigis.fetch_them_all()
+
+# generate historic gjsons
 import process.fetch.make_count_json
 import process.fetch.make_historic_villo_json

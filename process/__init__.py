c6b78dff8355099a9dbb49b485b0b43420513fb5
joren.sjongers <joren.sjongers@icloud.com>
2019-07-22 14:34:53 +0200

    update comments

--- process/__init__.py
+++ process/__init__.py
@@ -1,15 +1,18 @@
-import sys
+
 import os
-parent = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import sys
+parent = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parent)
 
 from config import get_config_by_env_mode
+
+# Initialize data directories
 get_config_by_env_mode().init_dirs()
 
 # fetch mobigis map data
-import process.fetch.fetch_mobigis as fetch_mobigis
+import process.fetch_mobigis.fetch_mobigis as fetch_mobigis
 fetch_mobigis.fetch_them_all()
 
-# generate historic gjsons
+# generate chart gjsons
 import process.fetch.make_count_json
 import process.fetch.make_historic_villo_json
